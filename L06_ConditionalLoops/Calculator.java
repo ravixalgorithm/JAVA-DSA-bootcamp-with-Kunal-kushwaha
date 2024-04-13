1ee2506dcd6d3a3e572c5dc1891c99d612a797ac
Mr. Algorithm <148683640+ravixalgorithm@users.noreply.github.com>
2024-04-14 00:27:31 +0530

    new

---
+++ L06_ConditionalLoops/Calculator.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // Take input from user till user does not press X or x
+        int ans = 0;
+        while (true) {
+            // take the operator as input
+            System.out.print("[Choose the Operator(+,-,*,/,%)] OR [X or x to Exit]: ");
+            char op = in.next().trim().charAt(0); // Trim() is used to make the string to Char
+
+            if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+                // input two numbers
+                System.out.println("Enter two numbers");
+                System.out.print("Num1:");
+                int num1 = in.nextInt();
+                System.out.print("Num2:");
+                int num2 = in.nextInt();
+
+                if (op == '+') {
+                    ans = num1 + num2;
+                }
+                if (op == '-') {
+                    ans = num1 - num2;
+                }
+                if (op == '*') {
+                    ans = num1 * num2;
+                }
+                if (op == '/') {
+                    if (num2 != 0) {
+                        ans = num1 / num2;
+                    }
+                }
+                if (op == '%') {
+                    ans = num1 % num2;
+                }
+            } else if (op == 'x' || op == 'X') {
+                break;
+            } else {
+                System.out.println("Invalid operation!!");
+            }
+            System.out.println(ans);
+        }
+    }
+}
