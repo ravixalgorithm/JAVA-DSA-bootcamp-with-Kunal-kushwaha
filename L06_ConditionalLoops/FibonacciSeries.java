b128788b1a155fd5429c3145ad16a6bd6eebd103
Mr. Algorithm <148683640+ravixalgorithm@users.noreply.github.com>
2024-05-26 16:03:25 +0530

    replacement and fibonacci code

---
+++ L06_ConditionalLoops/FibonacciSeries.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class FibonacciSeries {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int c = 0;
+        int a = 0;
+        int b = 1;
+        if (n==1){
+            System.out.println(a);
+        }
+        if(n==2){
+            System.out.println(b);
+        }
+        if (n>2) {
+            for (int i = 2; i < n; i++) {
+                c = a + b;
+                a = b;
+                b = c;
+
+            }
+            System.out.println(c);
+        }
+    }
+}
