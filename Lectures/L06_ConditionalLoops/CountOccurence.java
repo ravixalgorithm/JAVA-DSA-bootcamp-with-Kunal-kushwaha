37f5a69c48a35a7c1a0eca633509f86d9ae83b99
Mr. Algorithm <148683640+ravixalgorithm@users.noreply.github.com>
2024-05-26 17:10:02 +0530

    Digit Occurance Code

---
+++ Lectures/L06_ConditionalLoops/CountOccurence.java
@@ -0,0 +1,26 @@
+
+
+import java.util.Scanner;
+
+public class CountOccurence {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the Number: ");
+        long n = in.nextLong();
+        System.out.print("Enter the digit to find: ");
+        int d  = in.nextInt();
+
+        int count = 0;
+        while (n > 0) {
+            long sc = n % 10;
+            if (sc == d) {
+                count = count + 1;
+            }
+            n = n / 10;
+        }
+        System.out.println(d + " Occurs \"" + count + "\" Times");
+
+
+    }
+
+}
